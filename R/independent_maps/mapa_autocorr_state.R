632a6762e38181af135012bf47cc106c260dd35f
iskarwaluyo <iskarwaluyo@gmail.com>
2021-10-08 21:06:15 -0500

    OCT0821

---
+++ R/independent_maps/mapa_autocorr_state.R
@@ -0,0 +1,67 @@
+m7 <- leaflet(mun_mapa) %>%
+  addMapPane("A", zIndex = 490) %>% #
+  addMapPane("B", zIndex = 480) %>% # 
+  addMapPane("C", zIndex = 470) %>% # 
+  addMapPane("D", zIndex = 460) %>% # 
+  
+  addTiles() %>%
+  addTiles(group = "Open Street Map") %>%
+  addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+  addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+  addPolygons(stroke = FALSE, smoothFactor = 0.3, fillOpacity = .2,
+              fillColor = ~pal(as.numeric(CVE_MUN)),
+              label = ~paste0(NOMGEO, ": ", formatC(NOMGEO, big.mark = ",")))
+
+
+m7 <- m7 %>%  addPolygons(data = vegprim_autocor, stroke = TRUE, smoothFactor = 0.3,
+                          options = pathOptions(pane = "A"),
+                          fillOpacity = 1,
+                          fillColor = ~pal_autocorr(CL_PRI),
+                          opacity = .3,
+                          weight = 1,
+                          color = "#4D4D4D",
+                          dashArray = "2",
+                          highlight = highlightOptions(
+                            weight = 1,
+                            color = "#4D4D4D",
+                            fillOpacity = 0.1,
+                            dashArray = "2",
+                            bringToFront = TRUE),
+                          group = "AUTCORR VEGETACION PRIMARIA",
+                          labelOptions = labelOptions(
+                            style = list("font-weight" = "normal", padding = "3px 8px"),
+                            textsize = "15px",
+                            direction = "auto"))
+
+m7 <- m7 %>%  addPolygons(data = vegsecu_autocor, stroke = TRUE, smoothFactor = 0.3,
+                          options = pathOptions(pane = "A"),
+                          fillOpacity = 1,
+                          fillColor = ~pal_autocorr(CL_PCTSEC),
+                          opacity = .3,
+                          weight = 1,
+                          color = "#4D4D4D",
+                          dashArray = "2",
+                          highlight = highlightOptions(
+                            weight = 1,
+                            color = "#4D4D4D",
+                            fillOpacity = 0.1,
+                            dashArray = "2",
+                            bringToFront = TRUE),
+                          group = "AUTCORR VEGETACION SECUNDARIA",
+                          labelOptions = labelOptions(
+                            style = list("font-weight" = "normal", padding = "3px 8px"),
+                            textsize = "15px",
+                            direction = "auto"))
+
+
+# CONTROL DE CAPAS
+m7 <- m7 %>% addLayersControl(
+  baseGroups = c("Open Street Map", "Toner", "Toner Lite"),
+  overlayGroups = c("AUTCORR VEGETACION SECUNDARIA", "AUTCORR VEGETACION SECUNDARIA"),
+  options = layersControlOptions(collapsed = FALSE)
+)
+
+m7
+
+addLegend(m7, "topleft", group = "AUTCORR VEGETACION SECUNDARIA", pal = pal_autocorr, values = agricola_autocor$VPM_LISA_CL, opacity = 1.0, title = "Pressure Autocorrelation")
+
